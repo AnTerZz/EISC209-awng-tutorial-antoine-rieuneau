2c843261a2421d83bf2f80bf23678de5bf9800d2
Antoine Rieuneau <antoine.rieuneau@me.com>
2023-04-11 20:16:36 +0200

    Finished django tutorial 6

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -114,7 +114,11 @@ class QuestionDetailViewTests(TestCase):
         self.assertEqual(response.status_code, 404)
 
     def test_past_question2(self):
-        past_question = create_question(question_text='Past Question.', days=5)
+        """
+        The detail view of a question with a pub_date in the past
+        displays the question's text.
+        """
+        past_question = create_question(question_text='Past Question.', days=-5)
         url = reverse('polls:detail', args=(past_question.id,))
         response = self.client.get(url)
-        
+        self.assertContains(response, past_question.question_text)
